2f568878aa2d9fd44dfa07308bd230c541603807
Robert Howell Carr <rhc.iv@gst-mba.local>
2022-07-09 01:24:23 -0400

    Added more .md files

---
+++ 02 - Javascript Algorithms & Data Structures/Basic Javascript/01 - Comments.md
@@ -0,0 +1,19 @@
+## ***JavaScript Commenting***
+#### ***freeCodeCamp / JavaScript Algorithms & Data Structures / Basic Javascript***
+---
+JavaScript allows for both `inline` and `multi-line` comments as shown below.
+
+This is an example of an `inline` comment:
+```js
+//  This is an inline comment
+```
+This is an example of a `multi-line` comment:
+```js
+/* This is a multi-line comment and requires different annotation
+than that which is required for an otherwise inline comment */
+```
+---
+## ***Solution:***
+For an `inline` comment, use the following: `//`
+
+For a `multi-line` comment, open with `/*` and close with `*/`
--- 02 - Javascript Algorithms & Data Structures/Basic Javascript/basic-javascript.js
+++ 02 - Javascript Algorithms & Data Structures/Basic Javascript/basic-javascript.js
@@ -1,15 +1,3 @@
-// ! This is an inline comment.
-
-/* This is a multi-line comment in Javascript and requires different annotation than that which is found in a Javascripts inline comment */
-
-// ! Declare JavaScript Variables. 
-/* In computer science, data is anything that is meaningful to the computer. JavaScript provides eight different data types which are undefined, null, boolean, string, symbol, bigint, number, and object. */
-/* For example, computers distinguish between numbers, such as the number 12, and strings, such as "12", "dog", or "123 cats", which are collections of characters. Computers can perform mathematical operations on a number, but not on a string.Variables allow computers to store and manipulate data in a dynamic fashion. */
-/* They do this by using a "label" to point to the data rather than using the data itself. Any of the eight data types may be stored in a variable. Variables are similar to the x and y variables you use in mathematics, which means they're a simple name to represent the data we want to refer to. */
-/* Computer variables differ from mathematical variables in that they can store different values at different times. We tell JavaScript to create or declare a variable by putting the keyword var in front of it. */
-
-var myName;
-
 // ! Storing Values with the Assignment Operator. 
 /* In JavaScript, you can store a value in a variable with the assignment operator (=). If there are any calculations to the right of the = operator, those are performed before the value is assigned to the variable on the left of the operator.  */
 
